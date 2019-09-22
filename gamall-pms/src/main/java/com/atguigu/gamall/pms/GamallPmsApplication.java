fe6174de7b302401f1f784d319233c05059d44b4
andy <andy972019@aliyun.com>
2019-09-22 21:54:24 +0800

    谷粒商城初始化搭建

---
+++ gamall-pms/src/main/java/com/atguigu/gamall/pms/GamallPmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gamall.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamallPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamallPmsApplication.class, args);
+    }
+
+}
---
+++ gamall-pms/src/test/java/com/atguigu/gamall/pms/GamallPmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atguigu.gamall.pms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamallPmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
